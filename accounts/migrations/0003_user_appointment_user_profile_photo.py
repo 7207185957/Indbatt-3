c71a72c297547a100e77241e7896e9842f3cc0ca
7207185957 <41033555+7207185957@users.noreply.github.com>
2026-07-15 15:20:31 -0600

    Add profile fields after existing accounts migration

---
+++ accounts/migrations/0003_user_appointment_user_profile_photo.py
@@ -0,0 +1,21 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0002_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='user',
+            name='appointment',
+            field=models.CharField(blank=True, help_text='Current appointment, e.g. Adjutant or Company Commander.', max_length=100),
+        ),
+        migrations.AddField(
+            model_name='user',
+            name='profile_photo',
+            field=models.ImageField(blank=True, null=True, upload_to='profile_photos/'),
+        ),
+    ]
