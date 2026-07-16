83c83fcc1bee44538afebea92b533207c2b8c9d3
7207185957 <41033555+7207185957@users.noreply.github.com>
2026-07-16 09:37:12 -0600

    Add routes for unit module workspaces

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,4 +7,5 @@ app_name = 'dashboard'
 urlpatterns = [
     path('', views.UnitDashboardView.as_view(), name='home'),
     path('manpower/', views.ManpowerDashboardView.as_view(), name='manpower'),
+    path('module/<slug:module_slug>/', views.ModuleDashboardView.as_view(), name='module'),
 ]
